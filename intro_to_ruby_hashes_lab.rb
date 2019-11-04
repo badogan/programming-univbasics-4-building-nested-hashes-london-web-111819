f0a3541d836e717cf96cbd82c562440983d6c8eb
Basri Dogan <basri.dogan.71@gmail.com>
2019-11-04 16:10:40 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,8 +12,9 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   monopoly = {
-    :pieces=>4,
-    :railroads=>{}
+    :railroads=>{
+      :pieces=>4
+    }
     }
 end
 
@@ -23,8 +24,8 @@ def monopoly_with_third_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
    monopoly = {
-     :pieces=>4,
      :railroads=>{
+       :pieces=>4,
        :rent_in_dollars=>{
          :one_piece_owned=>25,
          :two_pieces_owned=>50,
